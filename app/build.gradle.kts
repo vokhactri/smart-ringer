010df180b6528cbf22f8de168a137ebf19dda1f2
Tri Vo Khac <git@trivk.dev>
2026-08-11 23:26:13 +0700

    fix: optimize icons for MagicOS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.trivk.smartringer"
         minSdk = 26
         targetSdk = 35
-        versionCode = providers.environmentVariable("VERSION_CODE").orNull?.toInt() ?: 3
-        versionName = providers.environmentVariable("VERSION_NAME").orNull?.removePrefix("v") ?: "1.1.1"
+        versionCode = providers.environmentVariable("VERSION_CODE").orNull?.toInt() ?: 4
+        versionName = providers.environmentVariable("VERSION_NAME").orNull?.removePrefix("v") ?: "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
